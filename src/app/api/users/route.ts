b0821138fb1e73ba942fb07f978b4b2d5264a9d0
Orlando <orlandokuanb@gmail.com>
2024-08-21 15:56:38 -0500

    fix router USER

--- src/app/api/users/route.ts
+++ src/app/api/users/route.ts
@@ -3,18 +3,3 @@ import type { NextApiRequest, NextApiResponse } from 'next';
 import axios from 'axios';
 
 const API_URL = process.env.API_URL_IA || 'https://orlandokuan.org';
-
-export default async function handler(req: NextApiRequest, res: NextApiResponse) {
-    if (req.method === 'GET') {
-        try {
-            const response = await axios.get(`${API_URL}/users`);
-            res.status(200).json(response.data);
-        } catch (error) {
-            console.error('Error fetching users:', error);
-            res.status(500).json({ message: 'Error fetching users' });
-        }
-    } else {
-        res.setHeader('Allow', ['GET']);
-        res.status(405).json({ message: `Method ${req.method} Not Allowed` });
-    }
-}
